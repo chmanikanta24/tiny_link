138c7fd3994c5a253a6a60c480d1c09d79b5ae71
chmanikanta24 <130432328+chmanikanta24@users.noreply.github.com>
2025-11-25 21:43:06 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { pool } from "./db.js";
-import linksRouter from "./routes/links.js";
+import linksRouter from "../routes/links.js";
 
 dotenv.config();
 
